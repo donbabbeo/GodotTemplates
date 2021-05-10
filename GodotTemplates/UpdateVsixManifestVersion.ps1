90102a94fe7800a34562cd1321dd809153b5847f
donbabbeo <donbabbeo@gmail.com>
2021-05-10 20:25:04 +0200

    Delete UpdateVsixManifestVersion.ps1

--- GodotTemplates/UpdateVsixManifestVersion.ps1
+++
@@ -1,8 +0,0 @@
-$version = $args[0]
-Write-Host "Set version: $version"
-
-$FullPath = Resolve-Path source.extension.vsixmanifest
-Write-Host $FullPath
-[xml]$content = Get-Content $FullPath
-$content.PackageManifest.Metadata.Identity.Version = $version
-$content.Save($FullPath
